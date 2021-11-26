9131b1334eb9ea8930e4b38ab51b4ed63104f510
Gean Antunes <gean.antunes@gmail.com>
2021-11-26 15:55:17 -0300

    primeiros exercicios de JS

---
+++ 01-FundamentosDevWeb/04-IntroducaoJavaScript-LogicaProgramacao/01b-JavaScript-PrimeirosPassos/scriptAula01.js
@@ -0,0 +1,5 @@
+const myName = "Gean Antunes";
+const birthCity = "Porto Alegre";
+let birthYear = 1985;
+
+console.log(myName,birthCity,birthYear);
---
+++ 01-FundamentosDevWeb/04-IntroducaoJavaScript-LogicaProgramacao/01b-JavaScript-PrimeirosPassos/scriptAula02.js
@@ -0,0 +1,19 @@
+// let patientId = 50;
+// let isEnrolled = true;
+// const patientInfo = {
+//   firstName: 'Ana',
+//   lastName: 'Santos',
+// };
+// const patientEmail = 'ana@email.com';
+
+// console.log(typeof patientAge);
+
+const base = 5;
+let height = 8;
+const area = base * height;
+
+console.log(area);
+
+const perimeter = (base + height) * 2;
+
+console.log(perimeter);
