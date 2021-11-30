ca6833774db31a6f933504cfb0c9e080df400b46
Gean Antunes <gean.antunes@gmail.com>
2021-11-29 23:15:53 -0300

    resolução do exercicio 06 do bloco 04-02

--- 01-FundamentosDevWeb/04-IntroducaoJavaScript-LogicaProgramacao/02-JavaScript-ArrayeLoopFor/scriptExercicio05.js
+++ 01-FundamentosDevWeb/04-IntroducaoJavaScript-LogicaProgramacao/02-JavaScript-ArrayeLoopFor/scriptExercicio05.js
@@ -1,7 +1,7 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let maior = 0;
+let maior = numbers[0];
 
-for (let i = 0; i < numbers.length; i += 1) {
+for (let i = 1; i < numbers.length; i += 1) {
   if (numbers[i] > maior) {
     maior = numbers[i];
   }
