6ffd9bad8147fc6c40619b21c49369a256c3ef00
Gean Antunes <gean.antunes@gmail.com>
2021-12-06 23:54:40 -0300

    resolução do exercício do Bk05-01

--- 01-FundamentosDevWeb/05-JavaScript--DOM-Eventos-WebStorage/01-JavaScript-DOM-Seletores/indexExercicio/script.js
+++ 01-FundamentosDevWeb/05-JavaScript--DOM-Eventos-WebStorage/01-JavaScript-DOM-Seletores/indexExercicio/script.js
@@ -10,3 +10,13 @@ Crie uma função que corrija o texto da tag <h1>.
 Crie uma função que modifique todo o texto da tag <p> para maiúsculo.
 Crie uma função que exiba o conteúdo de todas as tags <p> no console.
 */
+document.querySelectorAll(".center-content p")[1].innerText = "Eu serei um cara de muito sucesso e poderei comprar ifoods sem ficar pensando no salário";
+document.getElementsByClassName("main-content")[0].style.backgroundColor = "rgb(76,164,109)";
+document.getElementsByClassName("center-content")[0].style.backgroundColor = "#fff";
+document.getElementsByTagName("h1")[0].innerText = "Exercício 5.1 - JavaScript";
+document.querySelectorAll(".center-content")[0].style.textTransform = "uppercase";
+
+arr = document.getElementsByTagName("p");
+for (let i of arr) {
+  console.log(i.innerHTML);
+}
