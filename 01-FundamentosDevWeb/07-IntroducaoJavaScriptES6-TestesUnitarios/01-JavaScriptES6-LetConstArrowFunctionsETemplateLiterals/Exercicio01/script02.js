763fa13ec81ebfce971c5a33ffb21126bcaee6e6
Gean Antunes <gean.antunes@gmai.com>
2022-01-13 13:04:42 -0300

    resolução da primeira parte do exercicio02

--- 01-FundamentosDevWeb/07-IntroducaoJavaScriptES6-TestesUnitarios/01-JavaScriptES6-LetConstArrowFunctionsETemplateLiterals/Exercicio01/script02.js
+++ 01-FundamentosDevWeb/07-IntroducaoJavaScriptES6-TestesUnitarios/01-JavaScriptES6-LetConstArrowFunctionsETemplateLiterals/Exercicio01/script02.js
@@ -1,5 +1,5 @@
 const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-// Seu código aqui.
+const ordem = (oddsAndEvens) => oddsAndEvens.sort((a, b) => a - b);
 
-console.log(oddsAndEvens); // será necessário alterar essa linha 😉
+console.log(`Os números ${ordem(oddsAndEvens)} se encontram ordenados de forma crescente!`); // será necessário alterar essa linha 😉
