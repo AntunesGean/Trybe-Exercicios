013b43482bee911cd60fa92bb4b128c5132ac00e
Gean Antunes <gean.antunes@gmai.com>
2022-01-13 12:33:06 -0300

    funcion alterada para arrow e variavéis ajustadas para não vazar escopo

--- 01-FundamentosDevWeb/07-IntroducaoJavaScriptES6-TestesUnitarios/01-JavaScriptES6-LetConstArrowFunctionsETemplateLiterals/Exercicio01/script.js
+++ 01-FundamentosDevWeb/07-IntroducaoJavaScriptES6-TestesUnitarios/01-JavaScriptES6-LetConstArrowFunctionsETemplateLiterals/Exercicio01/script.js
@@ -12,16 +12,14 @@
 
 // testingScope(true);
 
-function testingScope(escopo) {
+const testingScope = (escopo) => {
   if (escopo === true) {
-    var ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-    console.log(ifScope);
+    const ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    console.log(`${ifScope} ótimo, fui utilizada no escopo !'`);
   } else {
-    var elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
     console.log(elseScope);
   }
-  console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
 }
 
 testingScope(true);
