90a83395378c20ab0fd1c6a24333e6c35ca63c13
Gean Antunes <gean.antunes@gmail.com>
2022-02-18 11:46:49 -0300

    ajustes para criação de novo bloco

--- 01-FundamentosDevWeb/05-JavaScript--DOM-Eventos-WebStorage/04-JavaScript-WebStorage/Exercicios/script.js
+++ 01-FundamentosDevWeb/05-JavaScript--DOM-Eventos-WebStorage/04-JavaScript-WebStorage/Exercicios/script.js
@@ -3,18 +3,18 @@ let btnBrnc = document.querySelectorAll(".colorTxt button")[1];
 let btnBlue = document.querySelectorAll(".colorTxt button")[2];
 
 btnPdr.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.color = "#000";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.color = "#000";
 });
 
 btnBrnc.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.color = "#fff";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.color = "#fff";
 });
 
 btnBlue.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.color = "#3340F6";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.color = "#3340F6";
 });
 
 let btnPqn = document.querySelectorAll(".fontSize button")[0];
@@ -22,18 +22,18 @@ let btnPadr = document.querySelectorAll(".fontSize button")[1];
 let btnGrnd = document.querySelectorAll(".fontSize button")[2];
 
 btnPqn.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontSize = "12px";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontSize = "12px";
 });
 
 btnPadr.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontSize = "16px";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontSize = "16px";
 });
 
 btnGrnd.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontSize = "20px";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontSize = "20px";
 });
 
 let btnPqnH = document.querySelectorAll(".spaceTxt button")[0];
@@ -41,18 +41,18 @@ let btnNrmH = document.querySelectorAll(".spaceTxt button")[1];
 let btnGrnH = document.querySelectorAll(".spaceTxt button")[2];
 
 btnPqnH.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.lineHeight = "80%";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.lineHeight = "80%";
 });
 
 btnNrmH.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.lineHeight = "normal";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.lineHeight = "normal";
 });
 
 btnGrnH.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.lineHeight = "200%";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.lineHeight = "200%";
 });
 
 let btnFmlyA = document.querySelectorAll(".fontFmly button")[0];
@@ -60,16 +60,16 @@ let btnFmlyB = document.querySelectorAll(".fontFmly button")[1];
 let btnGmlyC = document.querySelectorAll(".fontFmly button")[2];
 
 btnFmlyA.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontFamily = "Arial";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontFamily = "Arial";
 });
 
 btnFmlyB.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontFamily = "Times New Roman";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontFamily = "Times New Roman";
 });
 
 btnGmlyC.addEventListener("click", function() {
-  for (let i in document.getElementsByClassName("text"))
-  document.getElementsByClassName("text")[i].style.fontFamily = "Open Sans";
+  for (let i of document.getElementsByClassName("text"))
+  i.style.fontFamily = "Open Sans";
 });
