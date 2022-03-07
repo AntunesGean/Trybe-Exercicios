93294d28d3b6a7a1e2c75f11ed76f09426559dfa
Gean Antunes <gean.antunes@gmail.com>
2022-03-07 16:52:29 -0300

    exercicio01 pronto

---
+++ 02-DevFront-end/10-IntroducaoReact/01-Introducao-React/exercicio01/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './App.css';
+
+const Task = (value) => {
+  return (
+    <li key={value}>{value}</li>
+  );
+}
+
+const tarefas = ['Tomar banho', 'Tomar café', 'Ligar o PC', 'Estudar'];
+
+class App extends React.Component {
+  render() {
+    return (
+      <ul>{ tarefas.map(tarefa => Task(tarefa)) }</ul>
+    );
+  }
+}
+
+export default App;
