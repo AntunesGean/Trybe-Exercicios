abeb8fcbc0accb94733f1847549766cebc4b9337
Gean Antunes <gean.antunes@gmail.com>
2022-03-07 16:00:52 -0300

    primeiro componente em React

---
+++ 02-DevFront-end/10-IntroducaoReact/01-Introducao-React/hello-world/src/Component.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class HelloWorld extends React.Component {
+  render() {
+    return (
+      <div>
+        <h2>Meu primeiro componente de classe no React!</h2>
+        <p>Após um tempo sumido, estou de volta. Com muita vontade de aprender o React e detonar em Front End!</p>
+      </div>
+    )
+  }
+}
+
+export default HelloWorld;
