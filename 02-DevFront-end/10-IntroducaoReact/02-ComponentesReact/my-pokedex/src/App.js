6467babe326c0634c4a49451965e84dd9e7b9c5d
Gean Antunes <gean.antunes@gmail.com>
2022-03-07 22:31:12 -0300

    exercicio feito com ajuda do gabarito

---
+++ 02-DevFront-end/10-IntroducaoReact/02-ComponentesReact/my-pokedex/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import pokemons from './data';
+import React from 'react';
+import Pokedex from './pokedex';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className='App'>
+        <h1>Pokedex</h1>
+        <Pokedex pokemons={ pokemons }/>
+      </div>
+    );
+  }
+}
+
+export default App;
